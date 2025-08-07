b6dd8927f5108bf2b4c53b21edb398985daed8b3
gilliannieh <gilliannieh@gmail.com>
2025-08-06 20:41:11 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+// server/index.js
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from the Whisking backend!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
